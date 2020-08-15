213dd480f63426d2364889cf4c5885f0b8c5f177
unknown <evan.alonso1@gmail.com>
2020-08-15 17:00:00 +0200

    first commit

---
+++ tensor.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+import random
+
+import matplotlib.pyplot as plt
+import os
+import cv2
+#pip install opencv-python
+from tqdm import tqdm
+import numpy as np
+
+
+#https://github.com/ibab/tensorflow-wavenet/issues/255  solve directory problem
+
+NAME = "CNNopti"
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [1]
+layer_sizes = [32]
+conv_layers = [2]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            #tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      verbose = 2,
+                      callbacks = [tf.keras.callbacks.TensorBoard(log_dir="logs\{}".format(NAME), histogram_freq=1, profile_batch = 100000000)])
